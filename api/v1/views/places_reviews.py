8deb0a747f05bc91669412057c2b96e8bfcd7be7
abu al ameen <abulyaqs@gmail.com>
2024-01-07 18:13:20 +0100

    task 10

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from models import storage
 from models.place import Place
+from models.user import User
 from models.review import Review
 
 
